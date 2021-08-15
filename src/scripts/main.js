5dbb5622810cc31cb9ed771d8f0320ad6217c03c
IstiyakRiyad <istiyak.riyad@gmail.com>
2021-08-15 18:07:58 +0600

    Making some particle with connected lines

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,53 @@
 import "../styles/main.css";
+import { Vec2 } from "./math";
+import Particle from "./Particle";
+
+
+let particles;
+const NumberOfParticle = 100;
+
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const pixelRatio = window.devicePixelRatio;
+canvas.width = canvas.clientWidth * pixelRatio;
+canvas.height = canvas.clientHeight * pixelRatio;
+
+// Initializing particles
+function init() {
+    particles = [];
+    for(let i = 0; i < NumberOfParticle; i++) {
+        const position = new Vec2(Math.random() * canvas.width, Math.random() * canvas.height);
+        const velocity = new Vec2((Math.random() - .5)* 5, (Math.random() - .5) * 5);
+
+        particles.push(new Particle(position, velocity, "#000"));
+    }
+}
+
+
+// Animation loop function
+function animate(time) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    particles.forEach((particle, i) => {
+        particle.update(canvas, particles, i);
+        particle.draw(context);
+    });
+
+    requestAnimationFrame(animate);
+}
+requestAnimationFrame(animate);
+
+// Call init function
+init();
+
+// Checking for resize event listener
+addEventListener('resize', ()=> {
+    const pixelRatio = window.devicePixelRatio;
+
+    canvas.width = canvas.clientWidth * pixelRatio;
+    canvas.height = canvas.clientHeight * pixelRatio;
+
+    init();
+    console.log(canvas.width, canvas.height);
+});
