bf7fc56a8ac001f4131dd90f753c5566a2dba970
IstiyakRiyad <istiyak.riyad@gmail.com>
2021-08-21 21:16:44 +0600

    Adding Event Listener

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,18 @@ import "../styles/main.css";
 import { Vec2 } from "./math";
 import Particle from "./Particle";
 
+export const mouse = {
+    x: undefined,
+    y: undefined,
+    radius: undefined
+};
+export const RANGE = 100;
+export const LINE_WIDTH = 4;
 
+const numberOfParticle = 100;
+const color = '#000';
 let particles;
-const NumberOfParticle = 100;
+
 
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
@@ -16,11 +25,20 @@ canvas.height = canvas.clientHeight * pixelRatio;
 // Initializing particles
 function init() {
     particles = [];
-    for(let i = 0; i < NumberOfParticle; i++) {
-        const position = new Vec2(Math.random() * canvas.width, Math.random() * canvas.height);
-        const velocity = new Vec2((Math.random() - .5)* 5, (Math.random() - .5) * 5);
+    for(let i = 0; i < numberOfParticle; i++) {
+        const pX = Math.random() * canvas.width;
+        const pY = Math.random() * canvas.height;
+        const position = new Vec2(pX, pY);
 
-        particles.push(new Particle(position, velocity, "#000"));
+        let vX = (Math.random() - .5)* 2;
+        vX = (vX > 0) ? vX + 1 : vX - 1;
+
+        let vY = (Math.random() - .5) * 2;
+        vY = (vY > 0) ? vY + 1 : vY - 1;
+
+        const velocity = new Vec2(vX, vY);
+
+        particles.push(new Particle(position, velocity, color));
     }
 }
 
@@ -49,5 +67,33 @@ addEventListener('resize', ()=> {
     canvas.height = canvas.clientHeight * pixelRatio;
 
     init();
-    console.log(canvas.width, canvas.height);
+});
+
+canvas.addEventListener('mousemove', event => {
+    mouse.x = event.clientX;
+    mouse.y = event.clientY;
+    mouse.radius = (canvas.width / 80) * (canvas.height / 80);
+});
+
+canvas.addEventListener('mouseout', ()=> {
+    mouse.x = undefined;
+    mouse.y = undefined;
+});
+
+canvas.addEventListener('touchstart', event => {
+    mouse.x = event.touches[0].clientX * window.devicePixelRatio;
+    mouse.y = event.touches[0].clientY * window.devicePixelRatio;
+    mouse.radius = canvas.width / 8;
+});
+
+canvas.addEventListener('touchmove', event => {
+    mouse.x = event.touches[0].clientX * window.devicePixelRatio;
+    mouse.y = event.touches[0].clientY * window.devicePixelRatio;
+    mouse.radius = canvas.width / 8;
+});
+
+canvas.addEventListener('touchend', event => {
+    mouse.x = undefined;
+    mouse.y = undefined;
+    mouse.radius = undefined;
 });
